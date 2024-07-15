a70ac352e79f935be0dfc2599cf5002e90dc4183
Felipe Sousa <felipe.soares2007@hotmail.com>
2024-07-14 22:53:55 -0300

    including waiting steps

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import time
+
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 service = Service()
 
@@ -16,8 +21,32 @@ url = 'https://apnews.com/'
 driver.get(url)
 driver.maximize_window()
 
-driver.find_element(By.CLASS_NAME, 'SearchOverlay-search-button').click()
-driver.find_element(By.CLASS_NAME, 'SearchOverlay-search-input').send_keys('Trump')
+#setting wait conditions
+wait = WebDriverWait(driver, 5)
+
+#begin search
+phrase = 'economy'
+wait.until(EC.element_to_be_clickable((By.CLASS_NAME,'SearchOverlay-search-button'))).click()
+wait.until(EC.element_to_be_clickable((By.CLASS_NAME,'SearchOverlay-search-input'))).send_keys(phrase)
+# driver.find_element(By.CLASS_NAME, 'SearchOverlay-search-input').send_keys(phrase)  #searching phrase
 driver.find_element(By.CLASS_NAME, 'SearchOverlay-search-input').submit()
+
+
+drop_SortBy = driver.find_element(By.CLASS_NAME, 'Select-input')
+
+Select(webelement=drop_SortBy).select_by_visible_text('Newest')
+# Select(webelement=drop_SortBy).select_by_visible_text('Oldest')
+driver.refresh()
+
+time.sleep(3)
+
+# elements_titles = driver.find_elements(By.CLASS_NAME,'PagePromo-title')[6:]
+# elements_img = driver.find_elements(By.XPATH,'//img[@class="Image"]')[2:]
+#screenshot_as_base64
+
+# news_elements = driver.find_elements(By.CLASS_NAME,'PageList-items-item')[6:]
+news_elements = driver.find_elements(By.CLASS_NAME,'PagePromo-content')
+
+
 
 print('')
