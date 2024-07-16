576c5172de669c6ab7945a89de9052d8f547f5ba
Felipe Sousa <felipe.soares2007@hotmail.com>
2024-07-15 23:07:12 -0300

    generating excel file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import time
 import pandas as pd
 from selenium import webdriver
@@ -42,25 +43,20 @@ def main():
     # Select(webelement=drop_SortBy).select_by_visible_text('Oldest')
     driver.refresh()
 
-    Titles = []
-    Descriptions = []
-    Dates = []
-    imgs = []
-    moneys = []
-
     wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'SearchResultsModule-results')))
     time.sleep(3)
     news_elements = driver.find_elements(By.CLASS_NAME,'SearchResultsModule-results')
     news_elements = news_elements[0].find_elements(By.CLASS_NAME,'PageList-items-item')
     for item in news_elements:
         try:
-
             Title_element = item.find_element(By.CLASS_NAME,'PagePromoContentIcons-text').text
             date_element = item.find_element(By.CLASS_NAME,'PagePromo-date').text
             description_element = item.find_element(By.CLASS_NAME,'PagePromo-description').text
             img_element = item.find_element(By.CLASS_NAME,'PagePromo-media').screenshot_as_base64
+
         except:
             img_element = None
+
         Titles.append(Title_element)
         Descriptions.append(description_element)
         Dates.append(date_element)
@@ -71,6 +67,28 @@ def main():
 def hasMoney(string):
     return bool(re.search(r'(\$[0-9,]+(\.\d{2})?)|(USD|dollars)', string))
 
+Titles = []
+Descriptions = []
+Dates = []
+imgs = []
+moneys = []
+
+outputFolder = './Output'
+os.makedirs(outputFolder,exist_ok=True)
 
 main()
+
+data = {
+    'Title':Titles,
+    'Date':Dates,
+    'Description':Descriptions,
+    'Picture':'',
+    'Count phrases':'',
+    'Contain Money?':moneys
+    }
+
+df = pd.DataFrame(data)
+
+
+df.to_excel(f'./{outputFolder}/output.xlsx', index=False)
 print('End')
