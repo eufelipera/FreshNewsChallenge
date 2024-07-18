8f108a8f19dc5ae235c04763d1b49751e33d2d6d
Felipe Sousa <felipe.soares2007@hotmail.com>
2024-07-17 23:38:44 -0300

    downloading images

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import logging
 import re
+import base64 as b64
 
 def main():
     service = Service()
@@ -44,10 +46,11 @@ def main():
     driver.refresh()
 
     wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'SearchResultsModule-results')))
-    time.sleep(3)
+    time.sleep(5)
     news_elements = driver.find_elements(By.CLASS_NAME,'SearchResultsModule-results')
     news_elements = news_elements[0].find_elements(By.CLASS_NAME,'PageList-items-item')
-    for item in news_elements:
+
+    for count, item in enumerate(news_elements):
         try:
             Title_element = item.find_element(By.CLASS_NAME,'PagePromoContentIcons-text').text
             date_element = item.find_element(By.CLASS_NAME,'PagePromo-date').text
@@ -60,9 +63,19 @@ def main():
         Titles.append(Title_element)
         Descriptions.append(description_element)
         Dates.append(date_element)
-        imgs.append(img_element)
+        imgs.append(f'{outputFolder}/image{count}.jpeg')
         moneys.append(hasMoney(Title_element + description_element))
-    logging.info('End')
+
+        #decoding the image and writing it
+        b64ToImage(f'./{outputFolder}/image{count}.jpeg',img_element)
+    logging.info('All news has been captured!')
+
+def b64ToImage(path,content):
+    if content == None:
+        return
+    content = b64.b64decode(content)
+    with open(path,'wb') as file:
+        file.write(content)
 
 def hasMoney(string):
     return bool(re.search(r'(\$[0-9,]+(\.\d{2})?)|(USD|dollars)', string))
@@ -82,13 +95,14 @@ data = {
     'Title':Titles,
     'Date':Dates,
     'Description':Descriptions,
-    'Picture':'',
+    'Picture': imgs,
     'Count phrases':'',
     'Contain Money?':moneys
     }
 
+
+#create a dataframe
 df = pd.DataFrame(data)
 
 
 df.to_excel(f'./{outputFolder}/output.xlsx', index=False)
-print('End')
