f7042c5c370ad8264250608403429fcf5b8abc60
Felipe Sousa <felipe.soares2007@hotmail.com>
2024-07-21 22:09:53 -0300

    commit to production

--- tasks.py
+++ tasks.py
@@ -18,6 +18,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 @task
 def main(phrases):
+    
     # Initialize the Chrome driver service
     service = Service()
 
